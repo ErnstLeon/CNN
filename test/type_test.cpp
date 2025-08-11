4460738b7b56f69d5b6847398e7826d66e89b2c7
Leon Ernstberger <leon@Mac.fritz.box>
2025-08-11 22:22:49 +0200

    added more convolution application tests

--- test/type_test.cpp
+++ test/type_test.cpp
@@ -49,6 +49,58 @@ TEST(ConvolutionLayerTest, IdentApply) {
     }
 }
 
+TEST(ConvolutionLayerTest, PoolingApply) {
+
+    constexpr size_t CIN = 1, COUT = 1, K = 3, S = 1, P = 2;
+    CNN::Convolution_Layer<CIN, COUT, K, S, P, CNN::ReLU<float>> layer(0);
+
+    layer.kernels[K / 2 * K + K / 2] = 1;
+
+    CNN::Convolution_FeatureMap<CIN, 5, 5, float> input(2);
+    CNN::Convolution_FeatureMap<COUT, 3, 3, float> output;
+
+    layer.apply(input, output);
+
+    for (size_t i = 0; i < 2; ++i) {
+        EXPECT_FLOAT_EQ(static_cast<float>(2), output.features[i]);
+    }
+    EXPECT_FLOAT_EQ(static_cast<float>(1), output.features[2]);
+
+    for (size_t i = 3; i < 5; ++i) {
+        EXPECT_FLOAT_EQ(static_cast<float>(2), output.features[i]);
+    }
+    EXPECT_FLOAT_EQ(static_cast<float>(1), output.features[5]);
+
+    for (size_t i = 6; i < 8; ++i) {
+        EXPECT_FLOAT_EQ(static_cast<float>(1), output.features[i]);
+    }
+    EXPECT_FLOAT_EQ(static_cast<float>(0.5), output.features[8]);
+}
+
+TEST(ConvolutionLayerTest, StridingApply) {
+
+    constexpr size_t CIN = 1, COUT = 1, K = 3, S = 3, P = 1;
+    CNN::Convolution_Layer<CIN, COUT, K, S, P, CNN::ReLU<float>> layer(0);
+
+    layer.kernels[K / 2 * K + K / 2] = 1;
+
+    CNN::Convolution_FeatureMap<CIN, 5, 5, float> input(0);
+    CNN::Convolution_FeatureMap<COUT, 2, 2, float> output;
+
+    input.features[0] = static_cast<float>(2);
+    input.features[3] = static_cast<float>(1);
+
+    input.features[15] = static_cast<float>(2);
+    input.features[18] = static_cast<float>(1);
+
+    layer.apply(input, output);
+
+    EXPECT_FLOAT_EQ(static_cast<float>(2), output.features[0]);
+    EXPECT_FLOAT_EQ(static_cast<float>(1), output.features[1]);
+    EXPECT_FLOAT_EQ(static_cast<float>(2), output.features[2]);
+    EXPECT_FLOAT_EQ(static_cast<float>(1), output.features[3]);
+}
+
 TEST(ConvolutionLayerTest, OnesApply) {
 
     constexpr size_t CIN = 1, COUT = 1, K = 3, S = 1, P = 1;
@@ -119,8 +171,10 @@ TEST(NetworkBuildTest, Correct_LayerShapeDeduction)
     using Conv_Layers_Tuple = decltype(network.conv_layers);
     using Neural_Layers_Tuple = decltype(network.neural_layers);
 
-    using Conv_Features_Tuple = decltype(network.conv_features);
-    using Neural_Features_Tuple = decltype(network.neural_features);
+    using Conv_Features_Tuple = decltype(
+        CNN::featureMaps_from_layer<CHANNELS, IMG_HEIGHT, IMG_WIDTH>(network.conv_layers));
+    using Neural_Features_Tuple = decltype(
+        CNN::featureMaps_from_layer(network.neural_layers));
     
     static_assert(std::tuple_size_v<Conv_Layers_Tuple> == 2, 
         "Number of Conv layers in Network does not match input.");
