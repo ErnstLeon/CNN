5fde530b8d7a1b464011eba9145ea4f0cf660f23
Leon Ernstberger <leon@familie-ernstberger.de>
2025-09-04 10:54:47 +0200

    added L2 penalty for GD and weight decay for Adam

--- include/core/learning/optimization.hpp
+++ include/core/learning/optimization.hpp
@@ -15,14 +15,17 @@ private:
     static constexpr size_t size = Tensor::size;
 
     const T learning_rate = static_cast<T>(0.01);
+    const T penalty = static_cast<T>(1e-5);
 
 public:
     Gradient_Descent_() = default;
-    Gradient_Descent_(T learning_rate) : learning_rate(learning_rate) {}
+    Gradient_Descent_(T learning_rate, T penalty = static_cast<T>(1e-5)) 
+    : learning_rate(learning_rate), penalty(penalty) {}
 
     void update(Tensor& current, const Tensor& gradient) {
         for(size_t i = 0; i < size; ++i){
             current[i] -= learning_rate * gradient[i];
+            current[i] -= learning_rate * penalty * current[i];
         }
     }
 };
@@ -31,14 +34,16 @@ template<typename T>
 class Gradient_Descent {
 public:
     const T learning_rate = static_cast<T>(0.01);
+    const T penalty = static_cast<T>(1e-5);
 
     Gradient_Descent() = default;
-    Gradient_Descent(T learning_rate) : learning_rate(learning_rate) {}
+    Gradient_Descent(T learning_rate, T penalty = static_cast<T>(1e-5)) 
+    : learning_rate(learning_rate), penalty(penalty) {}
 
     template<typename Tensor>
     requires std::is_same_v<typename Tensor::type, T>
     Gradient_Descent_<Tensor> Tensor_Optimizer() const {
-        return Gradient_Descent_<Tensor>(learning_rate);
+        return Gradient_Descent_<Tensor>(learning_rate, penalty);
     }
 };
 
@@ -49,6 +54,7 @@ private:
     static constexpr size_t size = Tensor::size;
 
     const T learning_rate = static_cast<T>(0.001);
+    const T penalty = static_cast<T>(1e-5);
     const T beta1 = static_cast<T>(0.9);
     const T beta2 = static_cast<T>(0.999);
     const T epsilon = static_cast<T>(1e-8);
@@ -62,11 +68,12 @@ public:
     Adam_Optimizer_() = default;
 
     Adam_Optimizer_(T learning_rate, 
+                T penalty = static_cast<T>(1e-5),
                 T beta1 = static_cast<T>(0.9),   
                 T beta2 = static_cast<T>(0.999),
                 T epsilon = static_cast<T>(1e-8)) 
-    : learning_rate{learning_rate}, beta1{beta1}, beta2{beta2}, 
-    epsilon{epsilon } {}
+    : learning_rate{learning_rate}, penalty{penalty}, 
+    beta1{beta1}, beta2{beta2}, epsilon{epsilon} {}
 
 
     void update(Tensor& current, const Tensor& gradient) 
@@ -86,7 +93,8 @@ public:
             T m_hat = m[i] / (static_cast<T>(1) - beta1_pow_t);
             T v_hat = v[i] / (static_cast<T>(1) - beta2_pow_t);
 
-            current[i] -= learning_rate * m_hat / (std::sqrt(v_hat) + epsilon);
+            current[i] -= learning_rate * (m_hat / (std::sqrt(v_hat) + epsilon));
+            current[i] -= learning_rate * penalty * current[i];
         }
     }
 
@@ -100,24 +108,26 @@ public:
 template<typename T>
 class Adam_Optimizer {
 public:
-    T learning_rate = static_cast<T>(0.001);
-    T beta1 = static_cast<T>(0.9);
-    T beta2 = static_cast<T>(0.999);
-    T epsilon = static_cast<T>(1e-8);
+    const T learning_rate = static_cast<T>(0.001);
+    const T penalty = static_cast<T>(1e-5);
+    const T beta1 = static_cast<T>(0.9);
+    const T beta2 = static_cast<T>(0.999);
+    const T epsilon = static_cast<T>(1e-8);
 
     Adam_Optimizer() = default;
 
     Adam_Optimizer(T learning_rate, 
+                T penalty = static_cast<T>(1e-5),
                 T beta1 = static_cast<T>(0.9),   
                 T beta2 = static_cast<T>(0.999),
                 T epsilon = static_cast<T>(1e-8)) 
-    : learning_rate{learning_rate}, beta1{beta1}, beta2{beta2}, 
-    epsilon{epsilon } {}
+    : learning_rate{learning_rate}, penalty{penalty}, 
+    beta1{beta1}, beta2{beta2}, epsilon{epsilon} {}
 
     template<typename Tensor>
     requires std::is_same_v<typename Tensor::type, T>
     Adam_Optimizer_<Tensor> Tensor_Optimizer() const {
-        return Adam_Optimizer_<Tensor>(learning_rate, beta1, beta2, epsilon);
+        return Adam_Optimizer_<Tensor>(learning_rate, penalty, beta1, beta2, epsilon);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -22,32 +22,27 @@ int main(int argc, char ** argv){
   auto training_data = CNN::Utility::load_tiny_imag_train<CHANNELS, IMG_HEIGHT, IMG_WIDTH, CATEGORIES, T>("data/tiny-imagenet-200/train", category_map);
   auto test_data = CNN::Utility::load_tiny_imag_test<CHANNELS, IMG_HEIGHT, IMG_WIDTH, CATEGORIES, T>("data/tiny-imagenet-200/val", category_map);
   
-  // For testing only a 10% subset
-  training_data.resize(10000);
-  test_data.resize(1000);
-  
   // CNN design
   //  - Convolution_Layer : 
   //      Input channels, Output channels, Kernel size, Stride, Pooling size
   //  - Neural_Layer : 
   //      Input neurons, Output neurons
-  CNN::Convolution_Layer<3, 9, 5, 1, 2, CNN::ReLU<T>> con_layer_1;
-  CNN::Convolution_Layer<9, 16, 5, 2, 2, CNN::ReLU<T>> con_layer_2;
-  CNN::Convolution_Layer<16, 32, 5, 2, 2, CNN::ReLU<T>> con_layer_3;
-  CNN::Neural_Layer<64, 64, CNN::ReLU<T>> neural_layer_1;
-  CNN::Neural_Layer<64, 200, CNN::Softmax<T>> neural_layer_2;
+  CNN::Convolution_Layer<3, 8, 3, 1, 2, CNN::ReLU<T>> con_layer_1;
+  CNN::Convolution_Layer<8, 16, 3, 1, 2, CNN::ReLU<T>> con_layer_2;
+  CNN::Convolution_Layer<16, 32, 3, 1, 2, CNN::ReLU<T>> con_layer_3;
+  CNN::Neural_Layer<128, 200, CNN::Softmax<T>> neural_layer_1;
 
   // CNN initialization
-  auto network = CNN::Network::network<CHANNELS, IMG_HEIGHT, IMG_WIDTH, 3, 2>(
-      con_layer_1, con_layer_2, con_layer_3, neural_layer_1, neural_layer_2);
+  auto network = CNN::Network::network<CHANNELS, IMG_HEIGHT, IMG_WIDTH, 3, 1>(
+      con_layer_1, con_layer_2, con_layer_3, neural_layer_1);
 
-  // CNN optimization with Adam and step size of 0.01
-  CNN::Optimizer::Adam_Optimizer<T> opt(0.01);
-  network.train(training_data, opt, 128, 100); 
+  // CNN optimization with Adam and step size and decay
+  CNN::Optimizer::Adam_Optimizer<T> opt(0.001, 0.001);
+  network.train(training_data, opt, 64, 100); 
 
   // Get error of trained network on test data
   auto [loss, error] = network.assess(test_data);
-  std::cout << "Training loss: " << loss << " training error: " << error << std::endl;
+  std::cout << "Test loss: " << loss << " test error: " << error << std::endl;
   
   // Get network output category for known image
   auto test_image = CNN::Utility::img_vec_convert<CHANNELS, IMG_HEIGHT, IMG_WIDTH>("data/banana.jpg");
